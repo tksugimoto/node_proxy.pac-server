1f2e145d894278e4445980765eed977c32b2cfde
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-06-22 09:21:28 +0900

    [Update] ファビコンのリクエストには空を返す

--- server.js
+++ server.js
@@ -9,30 +9,34 @@ var http = require('http'),
 	fs   = require("fs");
 
 http.createServer(function(req, res) {
-	console.log(`[${new Date().toLocaleString()}] ${req.url}`);
-	fs.exists(filename, function (exists) {
-		if (exists) {
-			fs.readFile(filename, "utf8", function (err, file) {
-				if (err) {
-					res.writeHead(404, {
-						"Content-Type": "text/plain"
-					});
-					res.write(`File Not Found\n${filename}\nerr: ${err}`);
-					res.end();
-				} else {
-					res.writeHead(200, {
-						"Content-Type": "text/plain"
-					});
-					res.write(file);
-					res.end();
-				}
-			});
-		} else {
-			res.writeHead(404, {
-				"Content-Type": "text/plain"
-			});
-			res.write(`File Not Found\n${filename}`);
-			res.end();
-		}
-	});
+	if (req.url === "/favicon.ico") {
+		res.end();
+	} else {
+		console.log(`[${new Date().toLocaleString()}] ${req.url}`);
+		fs.exists(filename, function (exists) {
+			if (exists) {
+				fs.readFile(filename, "utf8", function (err, file) {
+					if (err) {
+						res.writeHead(404, {
+							"Content-Type": "text/plain"
+						});
+						res.write(`File Not Found\n${filename}\nerr: ${err}`);
+						res.end();
+					} else {
+						res.writeHead(200, {
+							"Content-Type": "text/plain"
+						});
+						res.write(file);
+						res.end();
+					}
+				});
+			} else {
+				res.writeHead(404, {
+					"Content-Type": "text/plain"
+				});
+				res.write(`File Not Found\n${filename}`);
+				res.end();
+			}
+		});
+	}
 }).listen(port, host);
