d692bc9f7e594a3903ff7efdfc82465d79f937b9
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-10-06 23:39:45 +0900

    [Clean] ダブルクォーテーション -> シングルクォーテーション

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 
 const port = 1024;
-const host = "127.0.0.1";
-const filename = "./files/proxy.pac";
+const host = '127.0.0.1';
+const filename = './files/proxy.pac';
 
 console.info(`http://${host}:${port}/`);
 
 const http = require('http'),
-	fs   = require("fs");
+	fs   = require('fs');
 
 http.createServer(function(req, res) {
-	if (req.url === "/favicon.ico") {
+	if (req.url === '/favicon.ico') {
 		res.end();
 	} else {
 		console.info(`[${new Date().toLocaleString()}] ${req.url}`);
 		fs.exists(filename, function (exists) {
 			if (exists) {
-				fs.readFile(filename, "utf8", function (err, file) {
+				fs.readFile(filename, 'utf8', function (err, file) {
 					if (err) {
 						res.writeHead(404, {
-							"Content-Type": "text/plain",
+							'Content-Type': 'text/plain',
 						});
 						res.write(`File Not Found\n${filename}\nerr: ${err}`);
 						res.end();
 					} else {
 						res.writeHead(200, {
-							"Content-Type": "text/plain",
+							'Content-Type': 'text/plain',
 						});
 						res.write(file);
 						res.end();
@@ -32,7 +32,7 @@ http.createServer(function(req, res) {
 				});
 			} else {
 				res.writeHead(404, {
-					"Content-Type": "text/plain",
+					'Content-Type': 'text/plain',
 				});
 				res.write(`File Not Found\n${filename}`);
 				res.end();
